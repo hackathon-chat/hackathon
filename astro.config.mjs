3f700a21fe2b9018e6e987ae4797b858e368a222
hackathon-chat <hackathon1024@gmail.com>
2025-07-18 17:48:25 +0800

    feat: update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://hackathon.chat', // 你的自定义域名
-  base: '/', // 因为使用自定义域名，所以用根路径
-  output: 'static', // 确保是静态输出
+  site: 'https://hackathon.chat', // Your custom domain
+  base: '/', // Use root path for custom domain
+  output: 'static', // Ensure static output
   build: {
     assets: 'assets'
   }
